6f064f5dd5df8799e8c1bb5c8dd46bc908bac99c
Aleksandor <aftis.sansan@gmail.com>
2021-11-22 16:24:39 +0300

    lab5 add tmp

--- JavaScript/Todo.js
+++ JavaScript/Todo.js
@@ -1,7 +1,9 @@
 const addTaskBtn = document.getElementById("add-task-btn");
 const deskTaskInput = document.getElementById("description-task");
 const todosWrapper = document.querySelector(".todos-wrapper");
+const myforminput = document.querySelector(".submit_on_enter");
 
+const t = document.getElementById('tmp');
 let tasks ;
 
 let todoItemElements =[];
@@ -16,17 +18,55 @@ function Task(description) {
 }
 
 const createTemplate = (task,index) => {
-    return `
-                                   <div class="todo-item ${task.completed ? 'checked' : ''}">
-                                        <div class ="description">${task.description}</div>
-                                        <div class ="buttons">
-                                            <input onclick="completeTask(${index})" class=" btn-complete" type="checkbox" ${task.completed ? 'checked' : ''}>
-                                           <button onclick="deleteTask(${index})" class="bth-delete">Delete</button>
-                                        </div>
-                                   </div>
-`
+    let t_copy = t.content.cloneNode(true);
+
+    let div_todo_item = t_copy.getElementById('todo_item');
+    if (task.completed) {
+        div_todo_item.classList.add('checked');
+    }
+
+
+    let div_discription =  t_copy.getElementById('descript');
+    div_discription.innerText = task.description;
+
+
+    let onclick_button =  t_copy.getElementById('input_id');
+
+    onclick_button.onclick = function () {
+        completeTask(index);
+    }
+
+    if (task.completed) {
+        onclick_button.type = 'checked';
+    }
+
+    let delete_button = t_copy.getElementById('button_id');
+
+    delete_button.onclick = function () {
+        deleteTask(index);
+    }
+
+
+    return t_copy;
+
+
+
+
+
+
+//      return `
+//                                    <div class="todo-item ${task.completed ? 'checked' : ''}">
+//                                         <div class ="description">${task.description}</div>
+//                                         <div class ="buttons">
+//                                            <input onclick="completeTask(${index})" class=" btn-complete" type="checkbox" ${task.completed ? 'checked' : ''}>
+//                                            <button onclick="deleteTask(${index})" class="bth-delete">Delete</button>
+//                                         </div>
+//                                    </div>
+// `
 
 }
+
+
 
 const filterTasks = () => {
     const activeTasks = tasks.length && tasks.filter(item => item.completed === false);
@@ -40,7 +80,7 @@ const fillHtmlList = () => {
     if(tasks.length > 0 ) {
         filterTasks();
         tasks.forEach((item,index)=>{
-            todosWrapper.innerHTML += createTemplate(item, index);
+            todosWrapper.appendChild(createTemplate(item, index));
         });
         todoItemElements = document.querySelectorAll('.todo-item');
     }
@@ -78,6 +118,8 @@ const deleteTask = index => {
     },500)
 }
 
+
+
 addTaskBtn.addEventListener('click', () => {
     tasks.push(new Task(deskTaskInput.value));
     // console.log(tasks)
@@ -85,3 +127,13 @@ addTaskBtn.addEventListener('click', () => {
     fillHtmlList();
     deskTaskInput.value ="";
 })
+
+myforminput.addEventListener("keypress", function (e) {
+    if (e.keyCode === 13) {
+        tasks.push(new Task(deskTaskInput.value));
+        // console.log(tasks)
+        updateLocal();
+        fillHtmlList();
+        deskTaskInput.value = "";
+    }
+});
