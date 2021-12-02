854a1cfe37e784d66b804be20946bd16cb0ef7ef
Aleksandor <aftis.sansan@gmail.com>
2021-12-02 22:17:34 +0300

    lab6 start

---
+++ app.js
@@ -0,0 +1,17 @@
+
+    VANTA.NET({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xd6d3d3,
+    backgroundColor: #f2f2f8,
+    points: 16.00,
+    maxDistance: 35.00,
+    spacing: 16.00,
+    showDots: false
+})
