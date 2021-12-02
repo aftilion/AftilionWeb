5901defb83f8ec3016c00c402f5770e8244da870
Aleksandor <aftis.sansan@gmail.com>
2021-12-02 18:19:49 +0300

    add menu and end lab5

--- JavaScript/timer.js
+++ JavaScript/timer.js
@@ -5,6 +5,11 @@ let startTime = 0;
 })();
 
 
+function showMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
 window.onload = function() {
     let endTime = (new Date).getTime();
     let footer = document.querySelector('footer');
@@ -18,6 +23,17 @@ window.onload = function() {
             links[i].classList.add("header_now");
         }
     }
+
+    window.addEventListener('click', e => {
+        if (!e.target.matches('.dropbtn')) {
+            const dropdown = document.getElementsByClassName("dropdown-content");
+            if (dropdown[0].classList.contains('show')) {
+                dropdown[0].classList.remove('show');
+            }
+        }
+    })
+
+
 }
 // $('.list').on('click', 'li', function() {
 //     $(this).closest('.list').find('li').removeClass('active');
