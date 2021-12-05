bee9db2fda7cf2b34795358f058df9ae8ae2fe55
Aleksandor <aftis.sansan@gmail.com>
2021-12-06 01:07:12 +0300

    lab6-7

--- JavaScript/requests.js
+++ JavaScript/requests.js
@@ -1,4 +1,5 @@
 function sleep(milliseconds) {
+
     const date = Date.now();
     let currentDate = null;
     do {
@@ -11,37 +12,34 @@ window.addEventListener('load', () => {
     let id = 1;
     const preloader = document.getElementById("preloader");
     const cards = document.getElementById("cards");
-
     const button = document.getElementById("add-card");
+
+
+
     button.addEventListener('click', async function (e) {
         button.disabled = true;
-        const card_t = document.getElementById("one-card");
-        const card = card_t.content.cloneNode(true);
-        const card_title_h2 = card.querySelector("h2");
+        const card = document.getElementById("one-card").content.cloneNode(true);
         const card_body = card.getElementById("card-body");
         preloader.classList.remove('disabled');
+
         try {
             let response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
             if (response.ok) {
                 let json = await response.json();
-                console.log(json);
-                if (Object.keys(json).length) {
-                    card_title_h2.innerHTML = json.title;
-                    card_body.setAttribute('id', id);
-                    card_body.innerHTML = json.body;
-                    id = id + 1;
-                } else {
-                    alert("Error");
-                }
-                sleep(2000);
+                card_body.innerHTML = json.body;
+                id = id + 1;
+                sleep(1000);
                 preloader.classList.add('disabled');
                 cards.insertBefore(card, preloader);
-            } else {
-                alert("Error");
+            }
+            else
+            {
+                alert("Error1");
             }
             button.disabled = false;
-        } catch (error) {
-            alert("Error");
+        } catch (error)
+        {
+            alert("Error2");
             preloader.classList.add('disabled');
             button.disabled = false;
         }
